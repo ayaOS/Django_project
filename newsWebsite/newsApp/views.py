b040222048332f7f63312546257288338d58ff80
ayaOS <ayaali35.aa@gmail.com>
2016-02-22 22:52:09 +0200

    creating post details page

--- newsWebsite/newsApp/views.py
+++ newsWebsite/newsApp/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render, get_object_or_404
+from .models import Post , Comment
 
 # Create your views here.
 
@@ -7,3 +7,9 @@ def homePageForUser(request):
 	posts = Post.objects.order_by('-date')
 	context = {'posts':posts}
 	return render(request,'newsApp/homePageForUser.html',context)
+
+def postDetails(request , post_id):
+	post = get_object_or_404( Post, id=post_id)
+	comments = Comment.objects.order_by('-date')
+	context = {'post':post,'comments':comments}
+	return render(request,'newsApp/postDetails.html',context)
