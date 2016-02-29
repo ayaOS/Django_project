8c39d408cf2c3540543035c644371c442e4bfe22
Caroline Samy <carolinsamy94@gmail.com>
2016-02-29 23:39:37 +0200

    show more page

--- newsWebsite/newsApp/urls.py
+++ newsWebsite/newsApp/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
 	url(r'^home$',views.listing),
 	#url(r'^home$',views.homePageForUser),
 
-	url(r'^home/post/(?P<post_id>[0-9]+)$',views.postDetails),
+	url(r'^home/post/(?P<post_id>[0-9]+)',views.postDetails),
 	url(r'^cat/(?P<cat_id>[0-9]+)$',views.categry),
 
 
